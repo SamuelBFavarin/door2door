a98f98d5ab0bc4aea8bfd7eb0e6666a44236d626
Samuel Favarin <samuel.favarin@paper.co>
2023-03-22 23:52:11 -0300

    feat: start dbt structure

---
+++ transformation/models/silver/vehicle_update_events.sql
@@ -0,0 +1,14 @@
+{{ config(
+    materialized='table',
+    tags=['daily']
+)}}
+
+
+SELECT 
+    data.id AS event_id,
+    organization_id,
+    data.location.lat AS latitude,
+    data.location.lng AS longitude,    	
+    data.location.at AS created_at
+FROM source('bronze', 'api_events')
+WHERE event = "update"
